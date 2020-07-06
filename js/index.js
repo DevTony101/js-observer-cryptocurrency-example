b89445dd0e4c61cd0ec02a8756a42be6c3fa7b76
DevTony101 <tonycortesano@gmail.com>
2020-07-06 11:10:34 -0500

    Subscribing users to the currencies

--- js/index.js
+++ js/index.js
@@ -7,3 +7,8 @@ const ethereum = new CryptoCurrency("Ethereum");
 const users = [];
 users.push(new User("Jonas"));
 users.push(new User("Martha"));
+
+// Subscribe all users to the bitcoin currency
+users.forEach(u => bitcoin.subscribe(u));
+// Subscribe the first user to the ethereum currency
+ethereum.subscribe(users[0]);
