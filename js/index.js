01692f1bfc09435b8128583f2605c3a688e9ca12
DevTony101 <tonycortesano@gmail.com>
2020-07-06 10:59:48 -0500

    Creating new subjects and observers

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import CryptoCurrency from "./currency";
+import User from "./user";
+
+const bitcoin = new CryptoCurrency("Bitcoin");
+const ethereum = new CryptoCurrency("Ethereum");
+
+const users = [];
+users.push(new User("Jonas"));
+users.push(new User("Martha"));
