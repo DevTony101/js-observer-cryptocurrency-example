67541e5df6c22e6b3edd97296871724273f4d08f
DevTony101 <tonycortesano@gmail.com>
2020-07-05 19:55:56 -0500

    Adding observer classes

---
+++ js/observer/subject.js
@@ -0,0 +1,21 @@
+export default class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+
+  unsubscribe(observer, areEqual) {
+    if (typeof areEqual === "function") {
+      this.observers = this.observers.filters((o) => !areEqual(o, observer));
+    } else {
+      throw new Error("A function is required");
+    }
+  }
+
+  notify() {
+    this.observers.forEach((o) => o.update(this));
+  }
+}
