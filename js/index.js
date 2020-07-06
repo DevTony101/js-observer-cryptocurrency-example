e206a3453ccbaf2858fd0b4320807551c8c66d91
DevTony101 <tonycortesano@gmail.com>
2020-07-06 11:28:25 -0500

    Adding for loop and settimeout

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import CryptoCurrency from "./currency";
-import User from "./user";
+import CryptoCurrency from "./currency.js";
+import User from "./user.js";
 
 const bitcoin = new CryptoCurrency("Bitcoin");
 const ethereum = new CryptoCurrency("Ethereum");
@@ -9,6 +9,17 @@ users.push(new User("Jonas"));
 users.push(new User("Martha"));
 
 // Subscribe all users to the bitcoin currency
-users.forEach(u => bitcoin.subscribe(u));
+users.forEach((u) => bitcoin.subscribe(u));
 // Subscribe the first user to the ethereum currency
 ethereum.subscribe(users[0]);
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(
+    () => bitcoin.increment(Math.floor(Math.random() * 10)), // Random amount
+    Math.floor(Math.random() * 10000) // Random waiting time
+  );
+  setTimeout(
+    () => ethereum.decrement(Math.floor(Math.random() * 10)),
+    Math.floor(Math.random() * 10000)
+  );
+}
